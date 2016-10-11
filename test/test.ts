0b32737ec43ab3dcf7423b4a05a8affcf0315ce1
Pierre Krafft <kpierre@outlook.com>
2016-10-12 00:03:35 +0200

    Categorize tests

--- test/test.ts
+++ test/test.ts
@@ -79,129 +79,138 @@ function highestMaxValue<T extends Interval>(tree: IntervalTree<T>): Node<T> {
 }
 
 describe('Interval tree', () => {
-  it('should correctly insert into an empty tree', () => {
-    const tree = new IntervalTree<StringInterval>()
 
-    tree.insert(getInterval(50, 100, 'data'))
+  describe('insert', () => {
+    it('should correctly insert into an empty tree', () => {
+      const tree = new IntervalTree<StringInterval>()
 
-    const searchResult = tree.search(50, 100)
-    expect(searchResult[0].data).to.eql('data')
+      tree.insert(getInterval(50, 100, 'data'))
+
+      const searchResult = tree.search(50, 100)
+      expect(searchResult[0].data).to.eql('data')
+    })
+
+    it('should correctly insert into a node with the same key', () => {
+      const tree = new IntervalTree<StringInterval>()
+
+      tree.insert(getInterval(50, 150, 'data1'))
+      tree.insert(getInterval(50, 100, 'data2'))
+
+      let searchResult = tree.search(75, 100)
+      expect(searchResult.length).to.eql(2)
+      expect(searchResult[0].data).to.eql('data1')
+      expect(searchResult[1].data).to.eql('data2')
+
+      searchResult = tree.search(125, 150)
+      expect(searchResult.length).to.eql(1)
+      expect(searchResult[0].data).to.eql('data1')
+    })
+
+    it('should correctly insert into a left subtree', () => {
+      const tree = new IntervalTree<StringInterval>()
+
+      tree.insert(getInterval(50, 150, 'data1'))
+      tree.insert(getInterval(25, 100, 'data2'))
+
+      const searchResult = tree.search(75, 100)
+      expect(searchResult.length).to.eql(2)
+      expect(searchResult[0].data).to.eql('data2')
+      expect(searchResult[1].data).to.eql('data1')
+    })
+
+    it('should correctly insert into a right subtree', () => {
+      const tree = new IntervalTree<StringInterval>()
+
+      tree.insert(getInterval(50, 150, 'data1'))
+      tree.insert(getInterval(75, 100, 'data2'))
+
+      const searchResult = tree.search(85, 100)
+      expect(searchResult.length).to.eql(2)
+      expect(searchResult[0].data).to.eql('data1')
+      expect(searchResult[1].data).to.eql('data2')
+    })
+
+    it('should reject intervals where low > high', () => {
+      const tree = new IntervalTree<Interval>()
+      const high = 10
+      const low = 15
+
+
+      expect(() => tree.insert({ low, high })).to.throw(Error)
+    })
   })
 
-  it('should correctly insert into a node with the same key', () => {
-    const tree = new IntervalTree<StringInterval>()
+  describe('search', () => {
+    it('should return an empty array when searching an empty tree', () => {
+      const tree = new IntervalTree<StringInterval>()
 
-    tree.insert(getInterval(50, 150, 'data1'))
-    tree.insert(getInterval(50, 100, 'data2'))
-
-    let searchResult = tree.search(75, 100)
-    expect(searchResult.length).to.eql(2)
-    expect(searchResult[0].data).to.eql('data1')
-    expect(searchResult[1].data).to.eql('data2')
-
-    searchResult = tree.search(125, 150)
-    expect(searchResult.length).to.eql(1)
-    expect(searchResult[0].data).to.eql('data1')
+      const searchResult = tree.search(75, 150)
+      expect(searchResult.length).to.eql(0)
+    })
   })
 
-  it('should correctly insert into a left subtree', () => {
-    const tree = new IntervalTree<StringInterval>()
+  describe('delete', () => {
+    it('should return false when trying to delete from an empty tree', () => {
+      const tree = new IntervalTree<Interval>()
 
-    tree.insert(getInterval(50, 150, 'data1'))
-    tree.insert(getInterval(25, 100, 'data2'))
+      const isRemoved = tree.remove({low: 50, high: 100})
+      expect(isRemoved).to.eql(false)
+    })
 
-    const searchResult = tree.search(75, 100)
-    expect(searchResult.length).to.eql(2)
-    expect(searchResult[0].data).to.eql('data2')
-    expect(searchResult[1].data).to.eql('data1')
+    it('should correctly delete the root node', () => {
+      const tree = new IntervalTree<StringInterval>()
+
+      tree.insert(getInterval(75, 150, 'data'))
+
+      const isRemoved = tree.remove(getInterval(75, 150, 'data'))
+      expect(isRemoved).to.eql(true)
+      expect(tree.root).to.eql(undefined)
+    })
+
+    it('should correctly delete the data object on a node with multiple data objects', () => {
+      const tree = new IntervalTree<StringInterval>()
+
+      tree.insert(getInterval(50, 120, 'data1'))
+      tree.insert(getInterval(75, 100, 'data2'))
+      tree.insert(getInterval(75, 200, 'firstDataToRemove'))
+      tree.insert(getInterval(75, 150, 'secondDataToRemove'))
+
+      let searchResult = tree.search(50, 200)
+      expect(searchResult.length).to.eql(4)
+      expect(searchResult[0].data).to.eql('data1')
+      expect(searchResult[1].data).to.eql('data2')
+      expect(searchResult[2].data).to.eql('firstDataToRemove')
+      expect(searchResult[3].data).to.eql('secondDataToRemove')
+
+      let isRemoved = tree.remove(getInterval(75, 200, 'firstDataToRemove'))
+      expect(isRemoved).to.eql(true)
+
+      searchResult = tree.search(50, 200)
+      expect(searchResult.length).to.eql(3)
+      expect(searchResult[0].data).to.eql('data1')
+      expect(searchResult[1].data).to.eql('data2')
+      expect(searchResult[2].data).to.eql('secondDataToRemove')
+
+      isRemoved = tree.remove(getInterval(75, 150, 'secondDataToRemove'))
+      expect(isRemoved).to.eql(true)
+
+      searchResult = tree.search(50, 200)
+      expect(searchResult.length).to.eql(2)
+      expect(searchResult[0].data).to.eql('data1')
+      expect(searchResult[1].data).to.eql('data2')
+    })
   })
 
-  it('should correctly insert into a right subtree', () => {
-    const tree = new IntervalTree<StringInterval>()
+  describe('implementation details', () => {
+    it('should be sorted with in-order traversal', () => {
+      const sorted = isSorted(randomTree)
+      expect(sorted).to.eql(true)
+    })
 
-    tree.insert(getInterval(50, 150, 'data1'))
-    tree.insert(getInterval(75, 100, 'data2'))
-
-    const searchResult = tree.search(85, 100)
-    expect(searchResult.length).to.eql(2)
-    expect(searchResult[0].data).to.eql('data1')
-    expect(searchResult[1].data).to.eql('data2')
-  })
-
-  it('should return an empty array when searching an empty tree', () => {
-    const tree = new IntervalTree<StringInterval>()
-
-    const searchResult = tree.search(75, 150)
-    expect(searchResult.length).to.eql(0)
-  })
-
-  it('should return false when trying to delete from an empty tree', () => {
-    const tree = new IntervalTree<Interval>()
-
-    const isRemoved = tree.remove({low: 50, high: 100})
-    expect(isRemoved).to.eql(false)
-  })
-
-  it('should correctly delete the root node', () => {
-    const tree = new IntervalTree<StringInterval>()
-
-    tree.insert(getInterval(75, 150, 'data'))
-
-    const isRemoved = tree.remove(getInterval(75, 150, 'data'))
-    expect(isRemoved).to.eql(true)
-    expect(tree.root).to.eql(undefined)
-  })
-
-  it('should correctly delete the data object on a node with multiple data objects', () => {
-    const tree = new IntervalTree<StringInterval>()
-
-    tree.insert(getInterval(50, 120, 'data1'))
-    tree.insert(getInterval(75, 100, 'data2'))
-    tree.insert(getInterval(75, 200, 'firstDataToRemove'))
-    tree.insert(getInterval(75, 150, 'secondDataToRemove'))
-
-    let searchResult = tree.search(50, 200)
-    expect(searchResult.length).to.eql(4)
-    expect(searchResult[0].data).to.eql('data1')
-    expect(searchResult[1].data).to.eql('data2')
-    expect(searchResult[2].data).to.eql('firstDataToRemove')
-    expect(searchResult[3].data).to.eql('secondDataToRemove')
-
-    let isRemoved = tree.remove(getInterval(75, 200, 'firstDataToRemove'))
-    expect(isRemoved).to.eql(true)
-
-    searchResult = tree.search(50, 200)
-    expect(searchResult.length).to.eql(3)
-    expect(searchResult[0].data).to.eql('data1')
-    expect(searchResult[1].data).to.eql('data2')
-    expect(searchResult[2].data).to.eql('secondDataToRemove')
-
-    isRemoved = tree.remove(getInterval(75, 150, 'secondDataToRemove'))
-    expect(isRemoved).to.eql(true)
-
-    searchResult = tree.search(50, 200)
-    expect(searchResult.length).to.eql(2)
-    expect(searchResult[0].data).to.eql('data1')
-    expect(searchResult[1].data).to.eql('data2')
-  })
-
-  it('should be sorted with in-order traversal', () => {
-    const sorted = isSorted(randomTree)
-    expect(sorted).to.eql(true)
-  })
-
-  it('should have highest `max` value in root node', () => {
-    const highest = highestMaxValue(randomTree)
-    expect((randomTree.root as Node<StringInterval>).max).to.eql(highest.max)
-  })
-
-  it('should reject intervals where low > high', () => {
-    const tree = new IntervalTree<Interval>()
-    const high = 10
-    const low = 15
-
-
-    expect(() => tree.insert({ low, high })).to.throw(Error)
+    it('should have highest `max` value in root node', () => {
+      const highest = highestMaxValue(randomTree)
+      expect((randomTree.root as Node<StringInterval>).max).to.eql(highest.max)
+    })
   })
 
   describe('InOrder', () => {
